1dd5a96cafb9bed0549d9b883a407b358c8bc709
suporte202 <suporte202@suporte03>
2021-01-13 18:19:32 -0200

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/projetoyoutube/Video.java
@@ -0,0 +1,25 @@
+package projetoyoutube;
+public class Video implements AcoesVideo {
+    private String titulo;
+    private int avaliavao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void like() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
