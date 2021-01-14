f37fcad55245b0bf2470d254cce3db1dd1ac4b02
suporte202 <suporte202@suporte03>
2021-01-14 12:17:15 -0200

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/projetoyoutube/Video.java
+++ src/projetoyoutube/Video.java
@@ -6,19 +6,70 @@ public class Video implements AcoesVideo {
     private int curtidas;
     private boolean reproduzindo;
 
+    public Video(String titulo) {
+        this.titulo = titulo;
+        this.avaliavao = 1;
+        this.views = 0;
+        this.curtidas = 0;
+        this.reproduzindo = false;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAvaliavao() {
+        return avaliavao;
+    }
+
+    public void setAvaliavao(int avaliavao) {
+        this.avaliavao = avaliavao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
     @Override
     public void play() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.reproduzindo = true;
     }
 
     @Override
     public void pause() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.reproduzindo = false;
     }
 
     @Override
     public void like() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.curtidas++;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" + "titulo=" + titulo + ", avaliavao=" + avaliavao + ", views=" + views + ", curtidas=" + curtidas + ", reproduzindo=" + reproduzindo + '}';
     }
     
     
